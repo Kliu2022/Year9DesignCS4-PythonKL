66e7618696db32da2ccf18259f88c6093183e3be
Kevin Liu <kevin.liu@Kevin-Liu.local>
2018-09-14 09:54:46 -0400

    09/14/2018 Strings

--- StringExample.py
+++ StringExample.py
@@ -1,1 +1,12 @@
-name = "Kevin"
+name = input("What is your name: ")
+
+letters1 = name[0:2] #inclusive:exclusive
+
+print(letters1)
+
+length = len(name)
+
+print(length)
+
+for i in range(length):
+	print(name[i])
