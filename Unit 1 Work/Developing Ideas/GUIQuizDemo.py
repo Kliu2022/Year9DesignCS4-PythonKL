4a16c84a55554d6381cd9fae50ffa247100474e8
Kevin Liu <kevin.liu@Kevin-Liu.local>
2018-11-11 10:28:18 -0500

    .

---
+++ Unit 1 Work/Developing Ideas/GUIQuizDemo.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import math
+questionNumber = 0
+
+
+root = tk.Tk()
+question = tk.Text(root,  height = 10, width = 50, borderwidth=3, relief=tk.GROOVE)
+question.insert(tk.INSERT, "Question ")
+question.grid(row = 0, column = 0, columnspan = 2)
+
+answerA = tk.Button(root, text="Answer A")
+answerA.grid(row = 1, column = 0)
+answerB = tk.Button(root, text="Answer B")
+answerB.grid(row = 1, column = 1)
+answerC = tk.Button(root, text="Answer C")
+answerC.grid(row = 2, column = 0)
+answerD = tk.Button(root, text="Answer D")
+answerD.grid(row = 2, column = 1)
+root.mainloop()
