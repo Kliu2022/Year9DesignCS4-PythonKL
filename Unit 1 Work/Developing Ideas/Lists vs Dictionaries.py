d7bbf7c3ec6670c0335546101b917a93a5499791
Kevin Liu <kevin.liu@Kevin-Liu.local>
2018-11-11 10:03:51 -0500

    dictionaries

---
+++ Unit 1 Work/Developing Ideas/Lists vs Dictionaries.py
@@ -0,0 +1,19 @@
+questionList = ["Question 1: What is 1+2", "Question 2: What is 4-2", "Question 3: What is 2-3"]
+for i in questionList:
+	print(i)
+
+
+switcher = {
+	0: "Question 1: What is 2+2",
+	1: "Question 2: What is 2+4",
+	2: "Question 3: What is 2+5",
+	3: "Question 4: What is 2+67",
+	4: "Question 5: What is 2+7",
+	5: "Question 6: What is 2+8",
+	6: "Question 7: What is 2+9",
+	7: "Question 8: What is 2+20",
+
+}
+
+for j in range(7):
+	print(switcher.get(j))
